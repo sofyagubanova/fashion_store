668e3b9e00117b3a62f8ae9cb1d1b9f4cd7aaf4f
sofyagubanova <sonyg-home@mail.ru>
2017-06-06 15:54:36 +0700

    Update register.php

--- register.php
+++ register.php
@@ -8,25 +8,25 @@
                <h2><a class="post_ttl">Успешная регистрация</a></h2>
                <div id="basket" style="background:none;">
                   <?php 
-					$login=htmlspecialchars($_POST['login']);
-					$password=htmlspecialchars($_POST['password']);
-					$logins=file('files/loginsJ72hds93jSHW82q.txt', FILE_IGNORE_NEW_LINES);
+			$login=htmlspecialchars($_POST['login']);
+			$password=htmlspecialchars($_POST['password']);
+			$logins=file('files/loginsJ72hds93jSHW82q.txt', FILE_IGNORE_NEW_LINES);
 					
-					for ($i=0; $i<count($logins); $i++)
-					{
-						if ($login==$logins[$i])
-						{
-							$_SESSION['login_already_exist']=true;
-							echo '<script>location.href="registration.php"</script>';
-							exit;
-						}
-					}
+			for ($i=0; $i<count($logins); $i++)
+			{
+				if ($login==$logins[$i])
+				{
+					$_SESSION['login_already_exist']=true;
+					echo '<script>location.href="registration.php"</script>';
+					exit;
+				}
+			}
 					
-					file_put_contents('files/loginsJ72hds93jSHW82q.txt', $login."\n", FILE_APPEND);
-					file_put_contents('files/passwordsQ78F2hd82saFWAZ212.txt', $password."\n", FILE_APPEND);
-					$_SESSION['login']=$login;
-					echo '<p align="center" style="font-size:28px;color:#fff;font-weight:bold;"> Вы успешно зарегистрированы!</p>';
-					?>
+			file_put_contents('files/loginsJ72hds93jSHW82q.txt', $login."\n", FILE_APPEND);
+			file_put_contents('files/passwordsQ78F2hd82saFWAZ212.txt', $password."\n", FILE_APPEND);
+			$_SESSION['login']=$login;
+			echo '<p align="center" style="font-size:28px;color:#fff;font-weight:bold;"> Вы успешно зарегистрированы!</p>';
+		?>
                </div>
             </div>
          </div>
